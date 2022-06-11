9306dc6327a7b2108352d47f33fa996a966af144
RaphaelaGoulart <92813211+RaphaelaGoulart@users.noreply.github.com>
2022-06-11 07:35:34 -0300

    Update database.sql

--- estac3/docs/database.sql
+++ estac3/docs/database.sql
@@ -12,9 +12,26 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Exportação de dados foi desmarcado.
 
--- Exportação de dados foi desmarcado.
+-- Copiando estrutura do banco de dados para sistema
+CREATE DATABASE IF NOT EXISTS `sistema` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `sistema`;
+
+-- Copiando dados para a tabela sistema.estacionamento: ~1 rows (aproximadamente)
+/*!40000 ALTER TABLE `estacionamento` DISABLE KEYS */;
+REPLACE INTO `estacionamento` (`id_estac`, `nome_estac`, `quant_vaga`) VALUES
+	(1, 'Estacione Aqui', 110);
+/*!40000 ALTER TABLE `estacionamento` ENABLE KEYS */;
+
+-- Copiando dados para a tabela sistema.usuarios: 5 rows
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+REPLACE INTO `usuarios` (`id`, `nome`, `usuario`, `senha`, `email`, `nivel`, `ativo`, `cadastro`) VALUES
+	(3, 'Admin1', 'admin1', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'admin@demo.com.br', 2, 1, '2016-09-13 11:12:00'),
+	(5, 'Admin2', 'admin2', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'admin2@mail.com', 3, 1, '2019-04-11 00:00:00'),
+	(6, 'Admin3', 'admin3', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'admin3@mail.com', 4, 1, '2019-04-11 00:00:00'),
+	(1, 'bia', 'bia', 'bia3', NULL, 2, 1, NULL),
+	(2, 'rapha', 'rapha', 'rapha3', NULL, 1, 1, NULL);
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
